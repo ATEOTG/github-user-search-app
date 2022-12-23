37dce74e54728d00ccfe1bc42486aad712d2f7ff
ATEOTG <bryansantos9797@gmail.com>
2022-12-23 13:16:21 -0500

    assets folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import classes from "./Header.module.css";
+
+class Header extends Component {
+  render() {
+    return <header className=""></header>;
+  }
+}
+
+export default Header;
