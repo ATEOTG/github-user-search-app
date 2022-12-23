3ee9bcb26c04b752619a00a10f6df21639bbfa09
ATEOTG <bryansantos9797@gmail.com>
2022-12-23 17:01:12 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import { Fragment } from "react";
-import "./App.css";
+import { Component, Fragment } from "react";
+import classes from "./App.module.css";
 import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <Fragment>
-      <Header />
-    </Fragment>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLight: false,
+    };
+  }
+
+  toggleThemeHandler() {
+    this.setState((currState) => {
+      return { isLight: !currState.isLight };
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <main
+          className={classes.main}
+          style={{
+            backgroundColor: !this.state.isLight ? "#141d2f" : "#f6f8ff",
+          }}
+        >
+          <div className={classes.container}>
+            <Header
+              isLight={this.state.isLight}
+              isLightHandler={this.toggleThemeHandler.bind(this)}
+            />
+          </div>
+        </main>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
 import { Component } from "react";
 import classes from "./Header.module.css";
+import Moon from "../svg/Moon";
+import Sun from "../svg/Sun";
 
 class Header extends Component {
   render() {
-    return <header className=""></header>;
+    return (
+      <div className={classes["header-cont"]}>
+        <h1
+          className={classes.title}
+          style={{ color: !this.props.isLight ? "#ffffff" : "#222731" }}
+        >
+          devfinder
+        </h1>
+        <div className={classes["theme-cont"]}>
+          <p
+            className={classes["theme-text"]}
+            style={{ color: !this.props.isLight ? "#ffffff" : "#2b3442" }}
+          >
+            {!this.props.isLight ? "LIGHT" : "DARK"}
+          </p>
+          {!this.props.isLight ? (
+            <Sun isLightHandler={this.props.isLightHandler} />
+          ) : (
+            <Moon isLightHandler={this.props.isLightHandler} />
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
