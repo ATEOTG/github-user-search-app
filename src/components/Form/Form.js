b3204c330a5504d88b5fd7eec30f66e5a49fee67
ATEOTG <bryansantos9797@gmail.com>
2022-12-25 21:48:35 -0500

    added primary form display

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,8 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 import classes from "./Form.module.css";
-import SearchIcon from "../svg/SearchIcon";
 import styled from "styled-components";
+import SearchIcon from "../svg/SearchIcon";
+import FormDisplay from "./FormDisplay";
 
 const Input = styled.input`
   background-color: rgba(255, 255, 255, 0);
@@ -25,22 +26,126 @@ const Input = styled.input`
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.myRef = createRef();
+    this.dateMap = {
+      1: "Jan",
+      2: "Feb",
+      3: "Mar",
+      4: "Apr",
+      5: "May",
+      6: "Jun",
+      7: "Jul",
+      8: "Aug",
+      9: "Sep",
+      10: "Oct",
+      11: "Nov",
+      12: "Dec",
+    };
+    this.state = {
+      user: "",
+      name: "",
+      following: 0,
+      followers: 0,
+      repos: 0,
+      bio: "",
+      location: "",
+      joined: "",
+      twitter: "",
+      company: "",
+      blog: "",
+      image: "",
+    };
   }
+
+  async searchUser(user) {
+    const response = await fetch(`https://api.github.com/users/${user}`, {
+      headers: {
+        Authorization:
+          "Bearer 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",
+      },
+    });
+    const data = await response.json();
+
+    const date = data.created_at.split("-");
+    const dateArr = [date[0], this.dateMap[+date[1]], date[2].slice(0, 2)];
+    this.setState(() => {
+      return {
+        user: data.login,
+        name: data.name,
+        following: data.following,
+        followers: data.followers,
+        repos: data.public_repos,
+        bio: data.bio,
+        location: data.location,
+        joined: dateArr,
+        twitter: data.twitter_username,
+        company: data.company,
+        blog: data.blog,
+        image: data.avatar_url,
+      };
+    });
+    console.log(data);
+  }
+
+  userChangeHandler(e) {
+    e.preventDefault();
+    this.setState(() => {
+      return { user: this.myRef.current.value };
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.user !== this.state.user) {
+      this.searchUser(this.state.user);
+    }
+  }
+
+  componentDidMount() {
+    this.searchUser("octocat");
+  }
+
   render() {
     return (
-      <form
-        className={classes["form-search"]}
-        style={{ backgroundColor: !this.props.isLight ? "#1e2a47" : "#fefefe" }}
-      >
-        <SearchIcon />
-        <Input
+      <div className={classes["form-div"]}>
+        <form
+          className={classes["form-search"]}
+          style={{
+            backgroundColor: !this.props.isLight ? "#1e2a47" : "#fefefe",
+            boxShadow: !this.props.isLight
+              ? "none"
+              : "0 1rem 2rem rgb(43, 52, 66,.15)",
+          }}
+        >
+          <SearchIcon />
+          <Input
+            isLight={this.props.isLight}
+            type="text"
+            placeholder="Search GitHub username…"
+            ref={this.myRef}
+          />
+          <button
+            onClick={this.userChangeHandler.bind(this)}
+            className={classes["search-btn"]}
+          >
+            Search
+          </button>
+        </form>
+        <FormDisplay
           isLight={this.props.isLight}
-          type="text"
-          placeholder="Search GitHub username…"
+          user={this.state.user}
+          name={this.state.name}
+          followers={this.state.followers}
+          following={this.state.following}
+          repos={this.state.repos}
+          bio={this.state.bio}
+          location={this.state.location}
+          joined={this.state.joined}
+          twitter={this.state.twitter}
+          company={this.state.company}
+          blog={this.state.blog}
+          image={this.state.image}
         />
-        <button className={classes["search-btn"]}>Search</button>
-      </form>
+      </div>
     );
   }
 }
--- src/components/Form/FormDisplay.js
+++ src/components/Form/FormDisplay.js
@@ -2,7 +2,40 @@ import { Component } from "react";
 import classes from "./FormDisplay.module.css";
 
 class FormDisplay extends Component {
-  render() {}
+  render() {
+    return (
+      <div
+        className={classes["display-cont"]}
+        style={{
+          backgroundColor: !this.props.isLight ? "#1e2a47" : "#fefefe",
+          boxShadow: !this.props.isLight
+            ? "none"
+            : "0 1rem 2rem rgb(43, 52, 66,.15)",
+        }}
+      >
+        <div className={classes["display-primary-cont"]}>
+          <img
+            className={classes.image}
+            src={this.props.image}
+            alt="avatar pfp"
+          />
+          <div className={classes["display-primary"]}>
+            <h3 style={{ color: !this.props.isLight ? "#ffffff" : "#222731" }}>
+              {!this.props.name ? this.props.user : this.props.name}
+            </h3>
+            <p className={classes.username}>@{this.props.user}</p>
+            <p
+              className={classes.date}
+              style={{ color: !this.props.isLight ? "#ffffff" : "#4b6a9b" }}
+            >
+              Joined {this.props.joined[2]} {this.props.joined[1]}{" "}
+              {this.props.joined[0]}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FormDisplay;
